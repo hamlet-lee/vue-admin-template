3483a9c97f6ed2e4ea2c581389a47dd4d0695f72
Hamlet Lee <lisn@rd.netease.com>
2019-11-29 20:02:35 +0800

    FIX: 更新URL失败

--- src/views/Adder.vue
+++ src/views/Adder.vue
@@ -14,6 +14,10 @@ export default {
     params: {
       type: Object,
       default: () => ({})
+    },
+    noReload: {
+      type: Boolean,
+      default: () => (false)
     }
   },
   data() {
@@ -23,6 +27,7 @@ export default {
       autoRun: false
     }
   },
+  // 首次进入本组件（通过导航，或者通过手工输入URL）
   beforeRouteEnter(to, from, next) {
     // 不能用this, 可以 next ( vm => {  vm是组件实例 })
     // next( vm => {
@@ -31,9 +36,14 @@ export default {
     console.log('calling: beforeRouteEnter')
     next(vm => {
       // debugger
+      // vm.myParams = JSON.parse(JSON.stringify(to.query))
       vm.reload()
     })
   },
+  // 点击了RUN，或者通过 导航或手工输入URL
+  // 如果是点击了RUN，可以带上noReload = true
+  // 这样就不用刷新参数
+
   beforeRouteUpdate(to, from, next) {
     console.log('calling: before route update')
     if (to.params.noReload) {
@@ -51,7 +61,7 @@ export default {
   methods: {
     reload() {
       const query = this.$route.query
-      this.myParams = query
+      this.myParams = JSON.parse(JSON.stringify(query))
       // 结果置空
       this.myC = ''
       this.autoRun = (query.autoRun === 'true' || query.autoRun === true)
